46f9b81c416fa221dbcc2d294f0005f48dc705e2
Kabir-Istiak <78077422+Kabir-Istiak@users.noreply.github.com>
2021-04-06 11:29:22 +0600

    Add files via upload

---
+++ css_layout/CSS_LAYOUT.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JS Form</title>
+	<style>
+.header {
+  
+  border-color: black;
+  background-color: Lavender;
+  text-align: center;
+  height: 100px;
+  padding: 10px;
+   float: top;
+}
+.nav {
+	
+	height: 540px;
+	width: 18%;
+	position: left;
+	background-color: Orchid;
+	text-align: center;
+	float: left;
+}
+.nav a:hover{
+	display: block;
+	background-color: PaleGreen;
+  	color: black;
+}
+.content {
+	
+	height: 540px;
+	width: 75%;
+	position: right;
+	background-color: Aquamarine;
+	text-align: center;
+	float: right;
+}
+.footer {
+	border-style: solid;
+  background-color: cornsilk;
+ 
+  padding: 10px;
+  text-align: center;
+}
+ul {
+  list-style-type: square;
+  margin: 0;
+  padding: 0;
+  font-size: 30px;
+}
+</style>
+</head>
+<body>
+	<div class="header">
+		<h1> WELCOME </h1>	
+	</div>
+	<div class="nav">
+ 	<ul>
+  		<li><a href="">Home</a></li>
+  		<li><a href="">  News</a></li>
+  		<li><a href="">Contact</a></li>
+  		<li><a href="">About</a></li>
+	</ul> 
+		
+	</div>
+	<div class="content">
+		<h1> LIFE STYLE </h1>	
+		<p align="left" > Lifestyle is the interests, opinions, behaviours, and behavioural orientations of an individual, group, or culture. The term was introduced by Austrian psychologist Alfred Adler in his 1929 book, The Case of Miss R., with the meaning of "a person's basic character as established early in childhood".
+			<br>
+			<br>
+The definition of lifestyle is the way you live including your style, attitudes and possessions. When you have all luxury items and can buy whatever you want, this is an example of your lifestyle.
+</p>
+	</div>
+	<div class="footer">
+			<h1> &#169 Copyright 1998 </h1>	
+	</div>
+	
+</body>
+</html>
